f6f67d372335ad905aa9771af2d1f8727d55bce7
Jade <44099468+JadeGrey@users.noreply.github.com>
2021-10-13 13:02:50 -0500

    Create timeStop.js

---
+++ timeStop.js
@@ -0,0 +1,2 @@
+(function(){document.getElementsByClassName("UIButton UIButton--hero")[0].click();
+setTimeout(function(){for(var F = setTimeout(";"), i = 0; i < F; i++) clearTimeout(i)},  prompt("Milliseconds only", "500 minimum"););})();
